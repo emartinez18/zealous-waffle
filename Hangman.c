e1314407cd6f05749184f5df2060ad0d1f8893de
ENRIQUE SILVEIRA MARTINEZ <15201529@l1607027.portoalegre.pucrsnet.br>
2015-10-21 11:29:11 -0200

    #3

--- Hangman.c
+++ Hangman.c
@@ -7,40 +7,96 @@ https://en.wikipedia.org/wiki/Hangman_%28game%29
 
 void create(char s[], char w[]);
 void clear();
+void draw(int erros);
 
 void create(char s[], char w[]) {
-	int len = strlen(s);
-	int j;
-	for (j=0 ; j<len ; j++)
-		w[j] = '-';
-	w[len] = '\0';
+    int len = strlen(s);
+    int j;
+    for (j=0 ; j<len ; j++)
+        w[j] = '-';
+    w[len] = '\0';
 }
 
 void clear() {
-	// FIXME: deve ficar no início da tela
-	//int i;
-	//for ( i = 0 ; i < 20 ; i++ )
-	// printf("\n");
+    // FIXME: deve ficar no início da tela
+    //int i;
+    //for ( i = 0 ; i < 20 ; i++ )
+    // printf("\n");
+}
+
+void draw(int erros) {
+    printf("\n");
+    printf(" +-----+ \n");
+    printf(" | | \n");
+    switch(erros) {
+
+    case 0:
+        printf(" | \n");
+        printf(" | \n");
+        printf(" | \n");
+        break;
+
+    case 1:
+        printf(" | o \n");
+        printf(" | \n");
+        printf(" | \n");
+        break;
+
+    case 2:
+        printf(" | o \n");
+        printf(" | O \n");
+        printf(" | \n");
+        break;
+
+    case 3:
+        printf(" | o \n");
+        printf(" | /O \n");
+        printf(" | \n");
+        break;
+
+    case 4:
+        printf(" | o \n");
+        printf(" | /O\\ \n");
+        printf(" | \n");
+        break;
+
+    case 5:
+        printf(" | o \n");
+        printf(" | /O\\ \n");
+        printf(" | / \n");
+        break;
+
+    case 6:
+        printf(" | o \n");
+        printf(" | /O\\ \n");
+        printf(" | / \\ \n");
+        break;
+    }
+    printf(" | \n");
+    printf("/ \\ \n");
+
+    printf("\n\n");
 }
 
 int main() {
-	char secret[100];
-	char word[100];
+    char secret[100];
+    char word[100];
 
-	printf("Jogo da Forca, (c) 2015\n");
+    printf("Jogo da Forca, (c) 2015\n");
 
-	printf("Informe a palavra secreta: ");
-	scanf("%s", secret);
+    printf("Informe a palavra secreta: ");
+    scanf("%s", secret);
 
-	clear();
-	create(secret, word);
+    clear();
+    create(secret, word);
 
-	printf("[%s]\n", word);
+    printf("[%s]\n", word);
+    draw();
 
-	// TODO: completar o jogo
-	
-	printf("*FIM*\n");
+    // TODO: completar o jogo
 
-	return EXIT_SUCCESS;
+    printf("*FIM*\n");
+
+    return EXIT_SUCCESS;
 }
 
